690125ec12edc23861c9c928f6748642eb7dc7e3
Sam Pardy <sampardy@mac.dhcp.nd.edu>
2026-03-05 16:23:58 -0500

    fixed app.jsx and added components.jsx

--- modern-web-dev/src/App.jsx
+++ modern-web-dev/src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import MusicListParent from './Components/musicList/musicListParent'
-import AddSongParent from './Components/addSong/addSongParent'
-import PieChartParent from './Components/pieChart/pieChartParent'
+import Components from './Components/Components'
 
 function App() {
   return (
     <>
-      <MusicListParent />
-      <AddSongParent />
-      <PieChartParent />
+      <Components/>
     </>
   )
 }
---
+++ modern-web-dev/src/Components/Components.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MusicListParent from './musicList/musicListParent'
+import AddSongParent from './addSong/addSongParent'
+import PieChartParent from './pieChart/pieChartParent'
+
+function Components() {
+  return (
+    <>
+      <MusicListParent />
+      <AddSongParent />
+      <PieChartParent />
+    </>
+  )
+}
+
+export default Components
