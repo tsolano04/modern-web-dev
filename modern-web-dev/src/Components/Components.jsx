492a31ee6e152853c6016596851d37284acb9c0e
Tom Solano <tmsol@MacBook-Air-5.local>
2026-03-06 17:17:07 -0600

    routed components to main page

--- modern-web-dev/src/Components/Components.jsx
+++ modern-web-dev/src/Components/Components.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import MusicListParent from './musicList/musicListParent'
 import AddSongParent from './addSong/addSongParent'
 import PieChartParent from './pieChart/pieChartParent'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function Components() {
   return (
-    <>
-      <MusicListParent />
-      <AddSongParent />
-      <PieChartParent />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MusicListParent />} />
+        <Route path="/add-song" element={<AddSongParent />} />
+        <Route path="/pie-chart" element={<PieChartParent />} />
+      </Routes>
+    </Router>
   )
 }
 
