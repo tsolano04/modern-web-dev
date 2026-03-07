0dc90b1eb6b58b88807198311f77a34e2890d7e8
Tom Solano <tmsol@MacBook-Air-5.local>
2026-03-06 18:39:26 -0600

    resolved merge conflicts

--- modern-web-dev/src/Components/Components.jsx
+++ modern-web-dev/src/Components/Components.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import MusicListParent from './musicList/musicListParent'
 import AddSongParent from './addSong/addSongParent'
 import PieChartParent from './pieChart/pieChartParent'
+import Footer from './Footer/Footer'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function Components() {
   return (
-    <>
-      <MusicListParent />
-      <AddSongParent />
-      <PieChartParent />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MusicListParent />} />
+        <Route path="/addSong" element={<AddSongParent />} />
+        <Route path="/pieChart" element={<PieChartParent />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
